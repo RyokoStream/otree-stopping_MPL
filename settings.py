23eb8a491d6eeb73faf367d0407c88f7620d16a0
RyokoStream <147413513+RyokoStream@users.noreply.github.com>
2026-08-24 23:47:14 +0900

    Remove commented content from settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+SESSION_CONFIGS = [
+    dict(
+        name='part1_test',
+        display_name="パート1（CE測定）の単体テスト",
+        num_demo_participants=1,
+        app_sequence=['part1_ce'], # 今回作ったアプリのみ指定
+    ),
+]
