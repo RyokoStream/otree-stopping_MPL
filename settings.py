c6ef6d391674b8cc171ed02cf438ecc8488e1bed
RyokoStream <147413513+RyokoStream@users.noreply.github.com>
2026-08-25 00:07:42 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,8 @@ SESSION_CONFIGS = [
         name='part1_test',
         display_name="パート1（CE測定）の単体テスト",
         num_demo_participants=1,
-        LANGUAGE_CODE = 'ja'
-        app_sequence=['part1_ce'], # 今回作ったアプリのみ指定
+        app_sequence=['part1_ce'],
     ),
 ]
+
+LANGUAGE_CODE = 'ja'  # <--- dict の外側（一番下など）に移動してください
